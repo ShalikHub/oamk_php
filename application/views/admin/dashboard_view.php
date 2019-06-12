771a424aef3abd52ef912cde3289440a5576302e
shalikram <sapkotashalikram@gmail.com>
2019-06-12 08:55:47 +0300

    teacher dahboard displayed

---
+++ application/views/admin/dashboard_view.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Welcome to the Dashboard</title>
+</head>
+<body>
+
+<div id="container">
+  <h1>Welcome to the Admin Dashboard!</h1>
+
+</div>
+
+</body>
+</html>
