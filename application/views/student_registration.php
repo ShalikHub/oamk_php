912e9abf953175e3ee20d3662a71b11985bd9d3c
shalikram <sapkotashalikram@gmail.com>
2019-07-01 09:20:14 +0300

    dashboard fixing

--- application/views/student_registration.php
+++ application/views/student_registration.php
@@ -3,9 +3,17 @@
 <html>
 <head>
 <title>Student Registration form</title>
+<style type="text/css">
+body{
+  background-color: #3EB650;
+}
+</style>
 </head>
 
 <body>
+<h1>You don't have accout yet? register youself.</h1>
+
+
  <form method="post">
  <table width="600" align="center" border="1" cellspacing="5" cellpadding="5">
  <tr>
@@ -46,10 +54,12 @@
   <tr>
     <td colspan="2" align="center">
  <input type="submit" name="register" value="Register Me"/></td>
- <span class="float-center"><a href="<?= base_url() . 'index.php/user/login'; ?>" class="btn btn-primary">Login</a></span>
+ <span class="float-center"><a href="<?= base_url() . 'index.php/user/login'; ?>" class="btn btn-primary"><h1>you already have an account? click here to login <h1></a></span>
   </tr>
 </table>
 
  </form>
+
+
 </body>
 </html>
